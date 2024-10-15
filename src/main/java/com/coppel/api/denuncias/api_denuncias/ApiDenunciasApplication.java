45b3cfe428b5e034098e96d8e0276ed60dbd9608
sergio iribe <sergio.iribec@gmail.com>
2024-10-14 21:39:22 -0700

    cors fix

--- src/main/java/com/coppel/api/denuncias/api_denuncias/ApiDenunciasApplication.java
+++ src/main/java/com/coppel/api/denuncias/api_denuncias/ApiDenunciasApplication.java
@@ -12,16 +12,17 @@ public class ApiDenunciasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiDenunciasApplication.class, args);
 	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
+	
 
 }
 
-@Bean
-public WebMvcConfigurer corsConfigurer() {
-	return new WebMvcConfigurer() {
-		@Override
-		public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*").allowedHeaders("*");
-		}
-	};
-}
- 
